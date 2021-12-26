f6cbb86c0e1e2a3368a74f987e1ff01445e17490
datnguyen_technology <nguyenquocdat91@gmail.com>
2021-12-26 16:04:30 +0700

    brach_main

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-print("hello_git_hello_tonycodedao2")
+print("hello_git_hello_tonycodedao_brack")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
